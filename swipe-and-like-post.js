a2ed03e77d86c38dff847b9008f1f2675df34b7e
Sagar Bhadauriya <72752355+sagar-700@users.noreply.github.com>
2020-10-18 11:35:07 +0530

    Create swipe-and-like-post.js

---
+++ swipe-and-like-post.js
@@ -0,0 +1,10 @@
+var j=17000;
+for(let i=0;i<30;i++){
+setTimeout(()=>{
+document.getElementsByClassName("coreSpriteRightPaginationArrow")[0].click()
+setTimeout(()=>{
+document.getElementsByClassName("wpO6b ")[2].click()
+},5000)
+},j)
+j+=17000;
+}
